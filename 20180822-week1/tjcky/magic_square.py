7c4e9d7c211b1c23993f2968654e58d565dbbd5b
tjcky <tjcky@naver.com>
2018-08-27 00:10:11 +0900

    add sample

---
+++ 20180822-week1/tjcky/magic_square.py
@@ -0,0 +1,19 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+input1 = ['5 3 4', '1 5 8', '6 4 2']
+input2 = ['4 9 2', '3 5 7', '8 1 5']
+input3 = ['4 8 2', '4 5 7', '6 1 6']
+
+# https://repl.it/repls/AchingHarmlessSignature
+# Complete the formingMagicSquare function below.
+def formingMagicSquare(s):
+  print(s)
+
+if __name__ == '__main__':
+  formingMagicSquare(input1)
