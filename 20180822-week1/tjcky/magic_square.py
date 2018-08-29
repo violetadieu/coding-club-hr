25f9cb7a48a0b699329cf4d3fa504b7427602ac5
tjcky <tjcky@naver.com>
2018-08-29 23:33:44 +0900

    doing..

--- 20180822-week1/tjcky/magic_square.py
+++ 20180822-week1/tjcky/magic_square.py
@@ -12,10 +12,38 @@ input3 = ['4 8 2', '4 5 7', '6 1 6']
 
 # Complete the formingMagicSquare function below.
 def formingMagicSquare(s):
-  convertS = convertToIntArr(s)
+  convertedS = convertToIntArr(s)
+  
+  alternates = [9,8,7,6,5,4,3,2,1]
+  # 1. 첫 행이 15가 맞는지 확인한다.
+  if sum(convertedS[0]) != 15:
+    alternates = set(alternates) - set(convertedS[0])
 
-  print(normal)
-  print(malformed)
+    for alt in reversed(list(alternates)):
+      convertedS[0][0] = alt
+      if sum(convertedS[0]) == 15:
+        print(alt)
+        break
+      convertedS[0][1] = alt
+      if sum(convertedS[0]) == 15:
+        print(alt)
+        break
+      convertedS[0][2] = alt
+      if sum(convertedS[0]) == 15:
+        print(alt)
+        break
+      
+
+  # 2. 15가 아니라면, 같은 행 내에서 중복값이 있는지 확인한다.
+  # 3. 중복값이 없다면, 맨 큰 숫자를 15 - (나머지 숫자의 합) = 값으로 바꾼다.
+
+  # 4. 둘째 행이 15가 맞는지 확인한다.
+  # 5. 15가 아니라면, 같은 행 또는 1행까지 중복값이 있는지 확인한다.
+  # 6. 중복값이 없다면, 맨 큰 숫자를 15 - (나머지 숫자의 합)= 값으로 바꾼다.
+
+  # 7. 셋째 행이 15가 맞는지 확인한다.
+  # 8. 15가 아니라면, 같은 행 또는 2행까지 중복값이 있는지 확인한다.
+  # 9. 중복값이 
 
 # convert Str array To Int array
 def convertToIntArr(s):
@@ -23,7 +51,6 @@ def convertToIntArr(s):
   for row in s:
     convertedS.append(list(map(lambda x: int(x), row.split(' '))))
   return convertedS
-
 
 if __name__ == '__main__':
   formingMagicSquare(input1)
