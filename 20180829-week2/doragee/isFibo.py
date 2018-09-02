609a8f6b1d3676c9cff9b69d51fe364db7ff5eaf
doragee <young.kwon@yujinrobot.com>
2018-09-02 13:43:50 +0900

    add init file

---
+++ 20180829-week2/doragee/isFibo.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+import os
+import sys
+
+# Complete the solve function below.
+def solve(n):
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input())
+
+    for t_itr in range(t):
+        n = int(input())
+
+        result = solve(n)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
