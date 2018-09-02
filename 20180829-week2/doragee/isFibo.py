ca65098c36a3f4b9cceeed77a4c8d47e2c61c684
tjcky <tjcky@naver.com>
2018-09-02 14:35:19 +0900

    solve isFibo

--- 20180829-week2/doragee/isFibo.py
+++ 20180829-week2/doragee/isFibo.py
@@ -4,18 +4,29 @@ import os
 import sys
 
 # Complete the solve function below.
-def solve(n):
+def solve(numberList):
+  result = []
+  
+  for n in numberList:
+    isFibo = False
+    first, second = 1, 1
+    if (n < 1):
+      pass
+    if (n == 1):
+      isFibo = True
+      pass
+
+    isFibo = False
+    while (n >= second):
+      if (n == second):
+        isFibo = True
+        break
+      first, second = second, first + second
+    
+    r = 'isFibo' if isFibo else 'isNotFibo'
+    result.append(r)
+    
+  return result
 
 if __name__ == '__main__':
-    fptr = open(os.environ['OUTPUT_PATH'], 'w')
-
-    t = int(input())
-
-    for t_itr in range(t):
-        n = int(input())
-
-        result = solve(n)
-
-        fptr.write(result + '\n')
-
-    fptr.close()
+    print(solve([5,7,8]))
