d6f2124c3f26ea21dca7eb2b892ce3c943abc7d8
김동현 <akxldk2@naver.com>
2018-09-03 23:37:44 +0900

    Week2 Is This a BST?

---
+++ 20180829-week2/Little-Monkey/Is This a BST?.cpp
@@ -0,0 +1,23 @@
+
+struct Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+bool checkBST(Node* root) {
+    Node* now=root;//현재 위치의 노드
+    //왼쪽 판별
+    while(true){
+        if(now->data>now->left->data||now->data<now->right->data){//왼쪽이 now보다 작거나,오른쪽이 now보다 크면
+            return false;//false를 리턴
+        }
+        checkBST(now->right);//오른쪽 노드 판별
+        checkBST(now->left);//왼쪽 노드 판별
+        //반복
+        if(now== nullptr) {//마지막이면
+            break;//탈출
+        }
+    }
+    return true;
+}
