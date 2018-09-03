6ba3abd427a695a72a1353f6a58e5508e350e811
김동현 <akxldk2@naver.com>
2018-09-04 02:03:39 +0900

    week2 modify code

--- 20180829-week2/Little-Monkey/Is This a BST?.cpp
+++ 20180829-week2/Little-Monkey/Is This a BST?.cpp
@@ -12,8 +12,10 @@ bool checkBST(Node* root) {
         if(now->data>now->left->data||now->data<now->right->data){//왼쪽이 now보다 작거나,오른쪽이 now보다 크면
             return false;//false를 리턴
         }
-        checkBST(now->right);//오른쪽 노드 판별
-        checkBST(now->left);//왼쪽 노드 판별
+        if(!checkBST(now->right))
+            return false;//오른쪽 노드 판별
+        if(!checkBST(now->left))
+            return false;//왼쪽 노드 판별
         //반복
         if(now== nullptr) {//마지막이면
             break;//탈출
